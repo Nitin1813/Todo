819e6975c97127030ab0a268483ef26ff883cecd
Nitin Kumar <nitin.kumar.84625@gmail.com>
2021-12-08 23:31:38 +0530

    added fa

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {FaIndex, FaRegCalenderAlt, FaRegCalender} from "react-icons/fa";
+import {FaInbox, FaRegCalenderAlt, FaRegCalender} from "react-icons/fa";
 
 const Sitebar = () =>{
     return (<div className="sitebar">
         <p>sitebar</p>
     <div className="">
-        <FaIndex />Inbox</div>
+        <FaInbox />Inbox</div>
     <div className="">Today</div>
     <div className="">Next 7 days</div>
     
